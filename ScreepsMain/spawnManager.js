2a042f549bd289a7a8211a704a79b0062886b91f
MrTinker64 <avi.micah@gmail.com>
2023-11-14 07:50:59 -0800

    raised numbers again

--- ScreepsMain/spawnManager.js
+++ ScreepsMain/spawnManager.js
@@ -1,8 +1,8 @@
 var minNumberOfHarvesters = 2;
-var maxNumberOfHarvesters = 12;
+var maxNumberOfHarvesters = 14;
 var minimumNumberOfUpgraders = 1;
 var maxNumberOfUpgradersFromSource = 1;
-var maxNumberOfConstructioneers = 8;
+var maxNumberOfConstructioneers = 10;
 
 /** 
  * @param {string} role, 
