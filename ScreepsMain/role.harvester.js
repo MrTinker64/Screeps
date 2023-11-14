c3df4cffcd87fa3dfdc847739b50efd7af53cca7
MrTinker64 <avi.micah@gmail.com>
2023-11-13 16:54:37 -0800

    beta test was good, spread to all roles

--- ScreepsMain/role.harvester.js
+++ ScreepsMain/role.harvester.js
@@ -3,16 +3,7 @@ var utils = require('utilities');
 
 module.exports = {
     run: function(creep) {
-        // if creep is bringing energy to the controller but has no energy left
-        if (creep.memory.working == true && creep.store[RESOURCE_ENERGY] == 0) {
-            // switch state
-            creep.memory.working = false;
-        }
-        // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()) {
-            // switch state
-            creep.memory.working = true;
-        }
+        utils.checkWorkingState(creep);
         
         // if creep is supposed to transfer energy to the spawn
         if (creep.memory.working == true) {
